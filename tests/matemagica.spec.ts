4df2172c17c6c68f1c784380b9cfe368492052ad
GLeonBS <gabriel.leonbs@gmail.com>
2023-05-10 22:26:41 -0300

    Movendo pastas de testes

--- tests/matemagica.spec.ts
+++ tests/matemagica.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "@jest/globals"
-import { Matemagica } from "../Matemagica"
+import { Matemagica } from "../src/atividades/matemagica/Matemagica"
 
 describe('Contas', () => {
     const matemagica = new Matemagica()
